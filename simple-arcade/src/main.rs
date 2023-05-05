4d12cf84c0ce1bc4ef45b5e66c72c1f0f208a64f
NilsVreman <nils.vreman@gmail.com>
2023-05-05 14:27:44 +0200

    Think it works now

--- simple-arcade/src/main.rs
+++ simple-arcade/src/main.rs
@@ -4,11 +4,13 @@ use snake::SnakePlugin;
 use minesweeper::MinesweeperPlugin;
 use arcade_menu::MenuPlugin;
 use arcade_util::DefaultArcadePlugin;
+use arcade_popup::PopupPlugin;
 
 
 fn main() {
     App::new()
         .add_plugin(DefaultArcadePlugin)
+        .add_plugin(PopupPlugin)
         // Has to add MenuPlugin before SnakePlugin because the states are defined in MenuPlugin
         .add_plugin(MenuPlugin)
         .add_plugin(SnakePlugin)
